2f5fc83149ac0a437f983e89352a04acac02b834
chinhtran9 <74026117+chinhtran9@users.noreply.github.com>
2022-03-14 08:47:28 +1000

    Update seed_external_job.groovy

--- jobs-script/seed_external_job.groovy
+++ jobs-script/seed_external_job.groovy
@@ -1,4 +1,8 @@
-job('seed-external-job') {
+folder('container-folder') {
+    description('Folder containing all jobs for folder-a')
+}
+
+job('container-folder/seed-external-job') {
   scm {
       git('https://github.com/chinhtran9/HelloWorldChinh.git')
   }
