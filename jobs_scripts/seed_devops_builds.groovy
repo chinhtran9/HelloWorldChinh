ac150ae195a42a888cc01219c924ccc1977a7312
chinhtran9 <74026117+chinhtran9@users.noreply.github.com>
2022-03-14 16:08:00 +1000

    Create seed_devops_builds.groovy

---
+++ jobs_scripts/seed_devops_builds.groovy
@@ -0,0 +1,16 @@
+def folderName = 'devops_builds'
+def tfs = 'https://github.com/figaw/freestyle-to-pipeline-jenkins.git'
+
+folder(folderName) {
+    description('Folder containing all jobs for folder-a')
+}
+
+job(folderName) {
+  scm {
+      git(tfs)
+  }
+  steps {
+    dsl {
+      external('jobs_script/' + folderName + '/*.groovy')
+    }
+  }
