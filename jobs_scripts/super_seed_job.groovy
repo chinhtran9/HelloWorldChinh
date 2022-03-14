872b4c825c6b715f535421834cbf6f30dfb3ba23
chinhtran9 <74026117+chinhtran9@users.noreply.github.com>
2022-03-14 16:05:39 +1000

    Create super_seed_job.groovy

---
+++ jobs_scripts/super_seed_job.groovy
@@ -0,0 +1,12 @@
+def tfs = 'https://github.com/figaw/freestyle-to-pipeline-jenkins.git'
+def superSeedJobName = "super_seed_job"
+
+job(superSeedJobName) {
+  scm {
+      git(tfs)
+  }
+  steps {
+    dsl {
+      external('jobs_script/' + superSeedJobName + '/*.groovy')
+    }
+  }
