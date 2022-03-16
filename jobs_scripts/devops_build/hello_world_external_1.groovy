443d28811864eab7d38ea81c7bea757b839d975b
Chinh Tran <chinh@localhost.localdomain>
2022-03-17 09:24:39 +1000

    df

--- jobs_scripts/devops_build/hello_world_external_1.groovy
+++ jobs_scripts/devops_build/hello_world_external_1.groovy
@@ -1,7 +1,13 @@
+import groovy.transform.BaseScript
+@BaseScript MainScript mainScript
+
 def folderName = 'devops_build'
 def tfs = 'https://github.com/chinhtran9/HelloWorldChinh.git'
 
-job(devops_build + '/hello_world_external_1') {
+
+println "$meaningOfLife" 
+
+job(folderName + '/hello_world_external_1') {
   steps {
     shell('echo Hello 1, World External!')
   }
