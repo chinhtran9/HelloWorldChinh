8cc147859263ad8844b4d69623f7a39eb77f2b5c
Chinh Tran <chinh@localhost.localdomain>
2022-03-18 11:17:48 +1000

    base line

--- job_scripts/devops_build/hello_world_external_1.groovy
+++ job_scripts/devops_build/hello_world_external_1.groovy
@@ -1,7 +1,17 @@
 def folderName = 'devops_build'
 def tfs = 'https://github.com/chinhtran9/HelloWorldChinh.git'
 
-job(folderName + '/hello_world_external_1') {
+File scriptFile = new File(getClass().protectionDomain.codeSource.location.path)
+
+// This holds the file name like "myscript.groovy"
+def scriptName = scriptFile.getName()
+
+// strip the extension to get "myscript"
+def withoutExt = scriptName.take(scriptName.lastIndexOf('.'))
+
+println(withoutExt + 'sdfdfd')
+
+job(hello_world_external_1') {
   steps {
     shell('echo Hello 1, World External!')
   }
